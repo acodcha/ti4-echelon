d1711ab5644bfc098b38db93cf3c9d3f46ce8b70
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2021-12-30 21:25:37 -0800

    Update games.

--- source/Games.hpp
+++ source/Games.hpp
@@ -33,28 +33,55 @@ public:
     }
   }
 
+  struct iterator : public std::multiset<Game, Game::sort_by_most_recent_date>::iterator {
+    iterator(const std::multiset<Game, Game::sort_by_most_recent_date>::iterator i) noexcept : std::multiset<Game, Game::sort_by_most_recent_date>::iterator(i) {}
+  };
+
   struct const_iterator : public std::multiset<Game, Game::sort_by_most_recent_date>::const_iterator {
     const_iterator(const std::multiset<Game, Game::sort_by_most_recent_date>::const_iterator i) noexcept : std::multiset<Game, Game::sort_by_most_recent_date>::const_iterator(i) {}
+  };
+
+  struct reverse_iterator : public std::multiset<Game, Game::sort_by_most_recent_date>::reverse_iterator {
+    reverse_iterator(const std::multiset<Game, Game::sort_by_most_recent_date>::reverse_iterator i) noexcept : std::multiset<Game, Game::sort_by_most_recent_date>::reverse_iterator(i) {}
+  };
+  struct const_reverse_iterator : public std::multiset<Game, Game::sort_by_most_recent_date>::const_reverse_iterator {
+    const_reverse_iterator(const std::multiset<Game, Game::sort_by_most_recent_date>::const_reverse_iterator i) noexcept : std::multiset<Game, Game::sort_by_most_recent_date>::const_reverse_iterator(i) {}
   };
 
   std::size_t size() const noexcept {
     return data_.size();
   }
 
+  iterator begin() const noexcept {
+   return iterator(data_.cbegin());
+  }
+
   const_iterator cbegin() const noexcept {
    return const_iterator(data_.cbegin());
   }
 
-  const_iterator begin() const noexcept {
-   return cbegin();
+  reverse_iterator rbegin() const noexcept {
+   return reverse_iterator(data_.crbegin());
+  }
+
+  const_reverse_iterator crbegin() const noexcept {
+   return const_reverse_iterator(data_.crbegin());
+  }
+
+  iterator end() const noexcept {
+   return iterator(data_.cend());
   }
 
   const_iterator cend() const noexcept {
    return const_iterator(data_.cend());
   }
 
-  const_iterator end() const noexcept {
-   return cend();
+  reverse_iterator rend() const noexcept {
+   return reverse_iterator(data_.crend());
+  }
+
+  const_reverse_iterator crend() const noexcept {
+   return const_reverse_iterator(data_.crend());
   }
 
 private:
